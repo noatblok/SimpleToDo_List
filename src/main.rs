dc4910d37ab29c50856eafc1f93e531044492593
Leo Rooney <leorooney08@gmail.com>
2026-06-22 16:23:19 -0400

    Very basic UI functionality

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,48 @@
-use inquire::{Text, Select};
+use eframe::egui;
 
-const MENU_CHOICES: [&str; 5] = ["Add Item", "Remove Item", "List Items", "Change Item Status", "Exit"];
-struct ToDoItem {
-    task: String,
-    completed: bool
-}
 fn main() {
-    let mut items: Vec<ToDoItem> = vec![];
-    loop {
-        let ans = Select::new("What would you like to do?", MENU_CHOICES.to_vec())
-            .prompt();
-        match ans {
-            Ok(choice) => {
-                match choice {
-                    "Add Item" => add_item(&mut items),
-                    "Remove Item" => remove_item(&mut items),
-                    "List Items" => list_items(&items),
-                    "Change Item Status" => change_item_status(&mut items),
-                    "Exit" => break,
-                    _ => println!("An error occured, try again.")
-                }
-            },
-            Err(_) => println!("An error occured, try again.")
-        }
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "To-Do List",
+        native_options,
+        Box::new(|cc| Ok(Box::new(TODOEguiApp::new(cc))))
+    ).expect("An error occured");
+}
+
+#[derive(Default)]
+struct TODOEguiApp {
+    task_buf: String,
+    tasks: Vec<(String, bool)>
+}
+
+impl TODOEguiApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
     }
 }
 
-fn add_item(items: &mut Vec<ToDoItem>) {
-    clearscreen::clear().expect("Something went wrong clearing the terminal");
-    let ans = Text::new("What would you like to add?")
-        .prompt();
-    match ans {
-        Ok(answer) => {
-            items.push(ToDoItem{task: answer, completed: false})
-        },
-        Err(_) => println!("An error occured, try again.")
-    }
-}
-
-fn remove_item(items: &mut Vec<ToDoItem>) {
-    clearscreen::clear().expect("Something went wrong clearing the terminal");
-    let mut itemlist: Vec<&str> = vec![];
-    for item in items.iter() {
-        itemlist.push(&item.task)
-    }
-
-    let choice = Select::new("What would you like to remove?", itemlist).prompt();
-    match choice {
-        Ok(selected_item) => {
-            if let Some(removal_index) = items.iter().position(|item| item.task == selected_item) {
-                items.remove(removal_index);
-            } else {
-                println!("You fucked up... Somehow")
+impl eframe::App for TODOEguiApp {
+    fn ui(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show_inside(ui, |ui| {
+            ui.heading("To-Do App");
+            ui.text_edit_singleline(&mut self.task_buf);
+            if ui.button("Add").clicked() {
+                self.tasks.push((self.task_buf.clone(), false));
             }
-        },
-        Err(_) => println!("An error occured, try again.")
-    }
-}
-
-fn change_item_status(items: &mut Vec<ToDoItem>) {
-    clearscreen::clear().expect("Something went wrong clearing the terminal");
-    let mut itemlist: Vec<&str> = vec![];
-    for item in items.iter() {
-        itemlist.push(&item.task)
-    }
-
-    let choice = Select::new("Which task would you like to change the status of?", itemlist).prompt();
-    match choice {
-        Ok(selected_item) => {
-            let status_select = Select::new("What would you like to change the status to?", vec!["Complete", "Incomplete"]).prompt();
-            if let Some(selected_index) = items.iter().position(|item| item.task == selected_item) {
-                match status_select {
-                    Ok(selected_status) => {
-                        match selected_status {
-                            "Complete" => {
-                                items[selected_index].completed = true
-                            },
-                            "Incomplete" => {
-                                items[selected_index].completed = false
-                            },
-                            _ => println!("An error occured, try again.")
-                        }
-                    },
-                    Err(_) => println!("An error occured, try again.")
-                }
-            } else {
-                println!("You fucked up... Somehow")
+            let mut task_index: usize = 0;
+            let mut to_remove: Vec<usize> = vec![];
+            for task in &mut self.tasks {
+                ui.horizontal(|ui| {
+                    ui.label(format!("{} - {}", task.0, task.1));
+                    if ui.button("Delete").clicked() {
+                        to_remove.push(task_index);
+                    }
+                });
+                task_index += 1;
             }
-        },
-        Err(_) => println!("An error occured, try again.")
+            for index_to_remove in to_remove {
+                self.tasks.remove(index_to_remove);
+            }
+        });
     }
-}
-
-fn list_items(items: &Vec<ToDoItem>) {
-    clearscreen::clear().expect("Something went wrong clearing the terminal");
-    let mut out_text = String::new();
-    let mut i = 0;
-    for item in items {
-        let mut to_add = String::new();
-        if item.completed == true {
-            to_add.push_str("Completed")
-        } else {
-            to_add.push_str("Incomplete")
-        }
-        if i != items.iter().len() - 1 {
-            to_add.push_str("\n");
-        }
-        out_text.push_str(&format!("{} - {}", &item.task, to_add));
-        i += 1
-    }
-
-    println!("Your list contains:\n{}", out_text);
 }
